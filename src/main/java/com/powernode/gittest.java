ba363fc3f5e2be22052cee127e1bd265e35b43cd
lhw <123@qq.com>
2022-04-23 16:31:28 +0800

    second commit

--- src/main/java/com/powernode/gittest.java
+++ src/main/java/com/powernode/gittest.java
@@ -3,5 +3,6 @@ package com.powernode;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello second");
     }
 }
